85b2d63048a1e47ff773373a6dc99d6799d40e6d
Gareth <gar.carey@gmail.com>
2021-09-26 11:51:47 -0400

    Add comments

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
-  console.log("Script on!");
 
+  //fuction to count remaining characters after inputs 
   $('.new-tweet textarea').on('input', function() {
     const charCounter = $(this).val().length;
     let remainingChars = `${140 - charCounter}`;
 
     let counterView = $(".counter").text(remainingChars);
     
+    //determines the colour of the remaining charcters
     if (remainingChars < 0) {
       $(counterView).css("color", "red");
     } else {
